1878d56588828c76d8d8bec694a3b953f7d075ef
DamC0XT <DamienCOXT@protonmail.com>
2020-04-06 18:15:29 +0100

    Updated FYP forms

--- app.py
+++ app.py
@@ -3,93 +3,90 @@ import json
 import pandas as ps
 import requests
 from flask import Flask, request, render_template, redirect, url_for, jsonify
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String ,select
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, select
 from forms import SelectDateForm
 import _sqlite3
 import json
+from datetime import date
+from datetime import datetime
 
-
-
-
-app = Flask(__name__,static_url_path='/static')
-
-
-
+app = Flask(__name__, static_url_path='/static')
 
 app.config['SECRET_KEY'] = '18c228e2bb6d1a93c7dca14e0122ddd0ed46c221bbb086b988f06cdb219aa39d'
 
 
 @app.route('/')
 def index():
-
-  return render_template('index.html')
-
+    return render_template('index.html')
 
 
-@app.route('/historical',methods=['GET','POST'])
+@app.route('/historical', methods=['GET', 'POST'])
 def rainfall():
-  form = SelectDateForm()
-  dateFrom = 0
-  dateTo = 0
-  hist = 0
-  if request.method == 'POST':
-    dateFrom = request.form['date']
-    dateTo = request.form['dateto']
-    hist = request.form['historicalOptions']
-  if form.validate_on_submit():
-
-    return redirect(url_for('showData',dateFrom=dateFrom,dateTo=dateTo,hist=hist))
- 
-  return render_template('historical.html',form=form)
+    form = SelectDateForm()
+    dateFrom = date
+    dateTo = date
+    hist = ''
+    error = "Sorry Invalid"
+    if request.method == 'POST':
+        dateFrom = request.form['date']
+        dateTo = request.form['dateto']
+        hist = request.form['historicalOptions']
 
 
+    if form.date.data is None and form.dateto.data is None:
+      return render_template('historical.html', form=form)
 
 
-
-
-
-
-@app.route('/showdata/<dateFrom>/<dateTo>/<hist>',methods=['GET','POST'])
-def showData(dateFrom,dateTo,hist):
-
-  #Read date from csv and use form input to send the view
-  data = ps.read_csv('CorkAirport.csv')
-  Soil = data[['date', hist]]
-  Soil['date'] = ps.to_datetime(Soil['date'])
-  soilMask = (Soil['date'] > dateFrom) & (Soil['date'] <= dateTo)
-  myData = Soil.loc[soilMask]
-
-  myData.to_csv("/home/roidanomaly/ImpactPredictor/static/Graph.csv",sep='\t',index=False)
-
-  return render_template('showdata.html',myData=myData.values.tolist(),hist=hist,dateFrom=dateFrom,dateTo=dateTo)
+    if form.date.data is not None and form.dateto.data is not None:
+      return redirect(url_for('showData',dateTo=dateTo,dateFrom=dateFrom,hist=hist))
 
 
 
+
+
+    return render_template('historical.html', form=form)
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/showdata/<dateFrom>/<dateTo>/<hist>', methods=['GET', 'POST'])
+def showData(dateFrom, dateTo, hist):
+    # Read date from csv and use form input to send the view
+    data = ps.read_csv('CorkAirport.csv', parse_dates=[0])
+    Soil = data[['date', hist]]
+    Soil['date'] = ps.to_datetime(Soil['date'])
+    soilMask = (Soil['date'] > dateFrom) & (Soil['date'] <= dateTo)
+    myData = Soil.loc[soilMask]
+
+    myData.to_csv("/home/roidanomaly/ImpactPredictor/static/Graph.csv", sep='\t', index=False)
+
+    return render_template('showdata.html', myData=myData.values.tolist(), hist=hist, dateFrom=dateFrom, dateTo=dateTo)
 
 
 @app.route('/weather')
 def showWeather():
-  engine = create_engine('sqlite:///predictions.db',echo=True)
-  connection = engine.connect()
-  metadata = MetaData()
-  predictions = Table('predictions', metadata, autoload=True, autoload_with=engine)
+    engine = create_engine('sqlite:///predictions.db', echo=True)
+    connection = engine.connect()
+    metadata = MetaData()
+    predictions = Table('predictions', metadata, autoload=True, autoload_with=engine)
 
-  query = select([predictions])
+    # query = select([predictions])
+    todaysDate = date.today()
+    newQuery = "SELECT * FROM predictions WHERE DATE LIKE '{}%'".format(todaysDate)
+    ResultProxy = connection.execute(newQuery)
 
-  ResultProxy = connection.execute(query)
+    pred = ResultProxy.fetchall()
 
-  pred = ResultProxy.fetchall()
-  print(pred)
-
-
-  return render_template('weather.html',pred=pred)
-
-
-  
-
-
-
+    return render_template('weather.html', pred=pred)
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField , StringField, SelectField
+from wtforms import SubmitField , SelectField
+from wtforms.fields.html5 import DateField
 
 from wtforms.validators import DataRequired, Length
 
 class SelectDateForm(FlaskForm):
     historicalOptions = SelectField('Historical Options', choices=[('Temp', 'Temperature'), ('Rain', 'Rainfall'), ('Soil', 'Soil Temperature'), ('Grass', 'Min Grass Temperature'), ('Wind', 'WindSpeed')])
-    date = StringField('Date from',validators=[DataRequired()],render_kw={"placeholder": "01-jan-2010"})
-    dateto = StringField('Date to',validators=[DataRequired()],render_kw={"placeholder": "10-feb-2019"})
-    
+    date = DateField('Date from', format('%d-%b-%Y'))
+    dateto = DateField('Date to',format('%d-%b-%Y'))
     submit = SubmitField('Submit')
+
